c0e6b434cd3f9880aeecec7e2a5425859439ba70
bapendabatam <bapendabatamscript@gmail.com>
2025-12-09 22:28:38 +0700

    use workers real url

--- src/utils/server.js
+++ src/utils/server.js
@@ -1,8 +1,10 @@
 // presensi-frontend/src/utils/server.JSON
 
 //const WORKER_DOMAIN = '192.168.101.72:8787';
-const WORKER_DOMAIN = '192.168.0.20:8787';
+//const WORKER_DOMAIN = '192.168.0.20:8787';
+const WORKER_DOMAIN = 'presensi-backend.bapendabatam.workers.dev';
 export const API_URL = `https://${WORKER_DOMAIN}`;
 export const WS_URL = `wss://${WORKER_DOMAIN}`;
 //export const FRONTEND_ORIGIN = `https://192.168.101.72:8788`;
-export const FRONTEND_ORIGIN = `https://192.168.0.20:8788`;
+//export const FRONTEND_ORIGIN = `https://192.168.0.20:8788`;
+export const FRONTEND_ORIGIN = `presensi-frontend.bapendabatam.workers.dev`;
