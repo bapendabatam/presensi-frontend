7439a8a2dee1590f31ba555f779632f97576304b
bapendabatam <bapendabatamscript@gmail.com>
2025-12-10 13:33:03 +0700

    fix server.js

--- src/utils/server.js
+++ src/utils/server.js
@@ -6,7 +6,7 @@ const WORKER_DOMAIN = 'presensi-backend.bapendabatam.workers.dev';
 
 //export const FRONTEND_ORIGIN = `https://192.168.101.72:8788`;
 //export const FRONTEND_ORIGIN = `https://192.168.0.20:8788`;
-export const FRONTEND_ORIGIN = `presensi-frontend.bapendabatam.workers.dev`;
+export const FRONTEND_ORIGIN = `presensi-acara-bapendabatam.pages.dev`;
 
 export const API_URL = `https://${WORKER_DOMAIN}`;
 export const WS_URL = `wss://${WORKER_DOMAIN}`;
